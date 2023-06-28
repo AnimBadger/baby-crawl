9f611b60d7beef9cca31e83e998ae73a7bcf1b13
AnimBadger <anim.ansah.stephen@gmail.com>
2023-06-28 16:25:40 +0000

    normalize with test cases

---
+++ crawl.test.js
@@ -0,0 +1,30 @@
+const { normalizeURL } = require('./crawl.js');
+const { test, expect } = require('@jest/globals');
+
+test('normalizeURl', () => {
+  const input = 'https://blog.boot.dev/path';
+  const actual = normalizeURL(input);
+  const expected = 'blog.boot.dev/path';
+  expect(actual).toEqual(expected);
+});
+
+test('normalizeURl trailing slashes', () => {
+  const input = 'https://blog.boot.dev/path/';
+  const actual = normalizeURL(input);
+  const expected = 'blog.boot.dev/path';
+  expect(actual).toEqual(expected);
+});
+
+test('normalizeURl capitalize', () => {
+  const input = 'https://BLOG.boot.dev/path/';
+  const actual = normalizeURL(input);
+  const expected = 'blog.boot.dev/path';
+  expect(actual).toEqual(expected);
+});
+
+test('normalizeURl http protocol', () => {
+  const input = 'https://blog.boot.dev/path/';
+  const actual = normalizeURL(input);
+  const expected = 'blog.boot.dev/path';
+  expect(actual).toEqual(expected);
+});
