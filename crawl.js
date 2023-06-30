64dc98d338ad49e1e2cf7c34c75cbf6baccf0ea3
AnimBadger <anim.ansah.stephen@gmail.com>
2023-06-30 02:08:56 +0000

    crawl page recursive

--- crawl.js
+++ crawl.js
@@ -1,24 +1,41 @@
 const { JSDOM } = require('jsdom');
 
-async function crawlPage (currentURL) {
+async function crawlPage (baseURL, currentURL, pages) {
+  const baseURLObj = new URL(baseURL);
+  const currentURLObj = new URL(currentURL);
+
+  if (baseURLObj.hostname !== currentURLObj.hostname) {
+    return pages;
+  }
+  const normalizedCurrentURL = normalizeURL(currentURL);
+  if (pages[normalizedCurrentURL] > 0) {
+    pages[normalizedCurrentURL]++;
+    return pages;
+  }
+  pages[normalizedCurrentURL] = 1;
   console.log(`Actively crawling ${currentURL}`);
   try {
     const resp = await fetch(currentURL);
     // check for status code
     if (resp.status > 399) {
       console.log(`Error with status code: ${resp.status} fetching ${currentURL}`);
-      return;
+      return pages;
     }
     // check for content type
     const contentType = resp.headers.get('content-type');
     if (!contentType.includes('text/html')) {
       console.log(`Can not crawl non-html websites, content-type ${contentType}`);
-      return;
+      return pages;
     }
-    console.log(await resp.text());
+    const HTMLbody = await resp.text();
+    const nextURLs = getURLsFromHTML(HTMLbody, baseURL);
+    for (const nextURL of nextURLs) {
+      pages = await crawlPage(baseURL, nextURL, pages);
+    }
   } catch (err) {
     console.log(`Error in fetch ${err} on page ${currentURL}`);
   }
+  return pages;
 }
 
 function getURLsFromHTML (htmlbody, baseURL) {
